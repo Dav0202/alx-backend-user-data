95e6c0e82d7f2c50dc1f6749b8465f6358aefd11
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-26 22:54:17 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+import bcrypt
 
+
+def hash_password(password: str) -> bytes:
+    """ Returns encrypted password """
+    encoded_pass = password.encode()
+    pass_word = bcrypt.hashpw(encoded_pass, bcrypt.gensalt())
+
+    return pass_word
