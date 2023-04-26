22f8e96b2147fd17ca85bb7d46229a65f9e6e3ba
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-26 12:58:25 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+import logging
+import re
+from typing import List
 
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for i in fields:
+        message = re.sub(f'{i}=.*?{separator}',
+                         f'{i}={redaction}{separator}', message)
+    return message
