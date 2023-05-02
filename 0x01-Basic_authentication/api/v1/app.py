6131d815941fd4ee99c36da3cbd10d61234bbea5
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-02 12:58:27 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -28,6 +28,11 @@ def unauthorized_error(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
