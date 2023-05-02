8924bd193b2505eee5c3c952643ac6d30a8dcef1
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-02 13:17:33 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
+""" Authentication Module
+"""
+from flask import request
 
+
+class Auth:
+    """ Class managing authentication of API """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method for checking if endpoint requires auth """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that handles authorization header """
+        if request is None:
+            return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Validates current user """
+        return None
