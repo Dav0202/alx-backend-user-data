9abe61b9dc7ea42c33b3018e49926a4e46aa843f
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-04 00:58:15 +0100

    Update user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+""" User session for Session in Db
+"""
+from models.base import Base
 
+
+class UserSession(Base):
+    """Class for user session
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """Constructor Method"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
