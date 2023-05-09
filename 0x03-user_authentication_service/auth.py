23f0d1207eea9ec648a721cd7d64dae63d977fed
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-09 12:15:29 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+""" Module for authentication """
 
+import bcrypt
+from db import DB
+from user import User
+from uuid import uuid4
+
+
+def _hash_password(password: str) -> str:
+    """ Returns a salted hash of the input password """
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
