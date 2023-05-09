46176c659320ca0d971f33d522f010680202c623
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-09 19:51:54 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,16 @@ class Auth:
             return True
 
         return False
+
+    def create_session(self, email: str) -> str:
+        """ Create sessions to database """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+
+        self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
