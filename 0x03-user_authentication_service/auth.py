a03762d8223bda7c96e8f6ace4bbde500575f0cc
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-10 13:23:34 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,10 +8,12 @@ from typing import Union
 from user import User
 from uuid import uuid4
 
+
 def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
 
 def _generate_uuid() -> str:
     """Returns string UUID"""
